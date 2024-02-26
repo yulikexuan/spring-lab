1f065860149e55bb363f599cfa5076f934e71f0c
Yu Li <yu.li@tecsys.com>
2024-02-26 08:51:12 -0500

    Instantiate Spring App with Builder

--- spring-container/src/main/java/springlab/SpringContainerLabApp.java
+++ spring-container/src/main/java/springlab/SpringContainerLabApp.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 @Slf4j
@@ -13,15 +14,30 @@ public class SpringContainerLabApp {
 
     public static void main(String[] args) {
 
+        log.info(">>> Instantiating SpringApplication Class ... ");
+
+        // instantiateSpringApplication(args);
+        instantiateSpringAppWithBuilder(args);
+    }
+
+    static void instantiateSpringApplication(final String[] args) {
+
         final var app = new SpringApplication(SpringContainerLabApp.class);
 
-        log.info(">>> Set up Spring Application ... ");
-
-        app.setHeadless(true);
+        app.setHeadless(false);
         app.setBannerMode(Banner.Mode.OFF);
         app.setLogStartupInfo(false);
 
         app.run(args);
+    }
+
+    static void instantiateSpringAppWithBuilder(final String[] args) {
+
+        new SpringApplicationBuilder(SpringContainerLabApp.class)
+                .headless(false)
+                .bannerMode(Banner.Mode.OFF)
+                .logStartupInfo(false)
+                .run(args);
     }
 
 }
